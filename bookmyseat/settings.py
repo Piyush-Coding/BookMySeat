50158c532c03a2312f7a32600774f664f36dc3e7
Piyush-Coding <piyushyadav19hiv1129@gmail.com>
2026-08-20 13:19:16 +0530

    Database ready

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -91,11 +91,24 @@ WSGI_APPLICATION = 'bookmyseat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Database Added Manually
+import os
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
